0ab5b82f73830497f28c2af33365145cc972c7d4
rherrajan <currant@gmx.de>
2014-10-26 14:11:58 +0100

    Deutlichere Befehle

--- increase-web/src/main/java/tk/icudi/LogProviderCurl.java
+++ increase-web/src/main/java/tk/icudi/LogProviderCurl.java
@@ -7,7 +7,7 @@ public class LogProviderCurl implements LogProvider {
 
 	public InputStream provideLogs() throws IOException {
 
-		String curlCMD = "cd ; development/callForLog.sh";
+		String curlCMD = "~/development/callForLog.sh";
 		String[] cmd = { "/bin/bash", "-c", curlCMD };
 		Process proc = Runtime.getRuntime().exec(cmd);
 
