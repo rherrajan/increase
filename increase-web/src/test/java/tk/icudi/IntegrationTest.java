3f4eb47db7e2c7dc6200c4423f91cdea2432fce9
rherrajan <currant@gmx.de>
2014-10-26 16:27:36 +0100

    umbenannt in Integrationtest

---
+++ increase-web/src/test/java/tk/icudi/IntegrationTest.java
@@ -0,0 +1,52 @@
+package tk.icudi;
+
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore("curls ingress. not to be used often")
+public class IntegrationTest {
+
+	@Test
+	public void test_parse() throws Exception {
+
+		PlextParser parser = new PlextParser(new LogProviderCurl());
+
+		parser.updateLogs();
+		List<LogEntry> logs = parser.extractLogEntries();
+
+		Assert.assertEquals(50, logs.size());
+
+		for (LogEntry logEntry : logs) {
+			System.out.println("logEntry: " + logEntry);
+		}
+	}
+
+	@Test
+	public void test_append() throws Exception {
+
+		Game game = new Game();
+
+		// game.start();
+
+		Thread.sleep(30 * 1000);
+		int portalsOnTime1 = game.getPortals().size();
+		for (Entry<Portal, String> entry : game.getPortals().entrySet()) {
+			System.out.println(entry.getValue() + " owns " + entry.getKey());
+		}
+
+		Thread.sleep(90 * 1000);
+		int portalsOnTime2 = game.getPortals().size();
+		for (Entry<Portal, String> entry : game.getPortals().entrySet()) {
+			System.out.println(entry.getValue() + " owns " + entry.getKey());
+		}
+
+		Assert.assertThat(portalsOnTime2, Matchers.greaterThan(portalsOnTime1));
+
+	}
+
+}
