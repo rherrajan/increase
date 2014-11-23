b490f138b7ffe2f3b83575faf9961ed91a199455
rherrajan <currant@gmx.de>
2014-11-23 11:51:35 +0100

    umbenannt

--- increase-appengine/src/main/java/tk/icudi/MainServlet.java
+++ increase-appengine/src/main/java/tk/icudi/MainServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class DemoServlet extends HttpServlet {
+public class MainServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
