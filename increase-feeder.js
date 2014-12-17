8549bb770e617cd7182392c5b068bc426c0aea8b
rherrajan <currant@gmx.de>
2014-12-17 16:06:16 +0100

    berechnung von Zeiten

--- increase-feeder.js
+++ increase-feeder.js
@@ -14,11 +14,13 @@ function wrapper(plugin_info) {
 if(typeof window.plugin !== 'function') window.plugin = function() {};
 
 // PLUGIN START ////////////////////////////////////////////////////////
-window.PLAYER_TRACKER_MIN_ZOOM = 9;
+
 
 // use own namespace for plugin
 window.plugin.increaseFeeder = function() {};
 
+window.plugin.increaseFeeder.minZoom = 9;
+    
 window.plugin.increaseFeeder.setup = function() {
   addHook('publicChatDataAvailable', window.plugin.increaseFeeder.handleData);
 };
@@ -28,18 +30,51 @@ window.plugin.increaseFeeder.stored = {};
 
 window.plugin.increaseFeeder.handleData = function(data) {
     
-    alert(" --- zoom: " + window.map.getZoom());
-    
-  if(window.map.getZoom() < window.PLAYER_TRACKER_MIN_ZOOM) {
+  if(window.map.getZoom() < window.plugin.increaseFeeder.minZoom) {
       return;
   }
 
+  var newestTime = 0;
+  var oldestTime = new Date().getTime() + 1000;
+    
   $.each(data.raw.success, function(ind, json) {
+      
+    var logTime = json[1];
+      
+      if(logTime > newestTime){
+          newestTime = logTime;
+      }
+      
+      if(logTime < oldestTime){
+          oldestTime = logTime;
+      }
+      
+    // alert(" --- logTime: " + logTime);
+      
     // skip old data
     // if(json[1] < limit) return true;
 
   });
+    
+    var ago = plugin.playerTracker.ago;
+    var now = new Date().getTime();
+    
+    alert(" --- length: " + data.raw.success.length);
+    alert(" --- zoom1: " + window.map.getZoom());
+    alert(" --- oldestTime: " + ago(oldestTime, now));
+    alert(" --- newestTime: " + ago(newestTime, now));
 
+}
+
+window.plugin.increaseFeeder.ago = function(time, now) {
+  var s = (now-time) / 1000;
+  var h = Math.floor(s / 3600);
+  var m = Math.floor((s % 3600) / 60);
+  var returnVal = m + 'm';
+  if(h > 0) {
+    returnVal = h + 'h' + returnVal;
+  }
+  return returnVal;
 }
 
 var setup = plugin.increaseFeeder.setup;
