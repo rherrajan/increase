593748705d3f66b85a1eae54bf5fde34095714e2
rherrajan <currant@gmx.de>
2015-01-02 19:24:38 +0100

    eigenes deploy skript

--- server/increase-systemtests/src/test/java/tk/icudi/FeederServletSystemTest.java
+++ server/increase-systemtests/src/test/java/tk/icudi/FeederServletSystemTest.java
@@ -1,6 +1,10 @@
 package tk.icudi;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -17,7 +21,7 @@ public class FeederServletSystemTest {
 		URL plexts = new URL("http://localhost:8080/feeder");
 		HttpURLConnection connection = (HttpURLConnection) plexts.openConnection();
 		
-		String result = PlextParser.streamToString(connection.getInputStream());
+		String result = streamToString(connection.getInputStream());
 				
 		Assert.assertThat(result, Matchers.notNullValue());
 	}
@@ -37,28 +41,19 @@ public class FeederServletSystemTest {
 		wr.close();
 		connection.disconnect();
 		
-		String result = PlextParser.streamToString(connection.getInputStream());
+		String result = streamToString(connection.getInputStream());
 				
 		Assert.assertThat(result, Matchers.notNullValue());
 	}
 	
-//	String urlParameters = "param1=a&param2=b&param3=c";
-//	String request = "http://example.com/index.php";
-//	URL url = new URL(request); 
-//	HttpURLConnection connection = (HttpURLConnection) url.openConnection();           
-//	connection.setDoOutput(true);
-//	connection.setDoInput(true);
-//	connection.setInstanceFollowRedirects(false); 
-//	connection.setRequestMethod("POST"); 
-//	connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); 
-//	connection.setRequestProperty("charset", "utf-8");
-//	connection.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
-//	connection.setUseCaches (false);
-//
-//	DataOutputStream wr = new DataOutputStream(connection.getOutputStream ());
-//	wr.writeBytes(urlParameters);
-//	wr.flush();
-//	wr.close();
-//	connection.disconnect();
-
+	private static String streamToString(InputStream inputStream) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+		StringBuilder builder = new StringBuilder();
+		String strLine;
+		while ((strLine = in.readLine()) != null) {
+			builder.append(strLine).append("\n");
+		}
+		return builder.toString();
+	}
+	
 }
