0f41d12ba9e3057579816727d2b3128a19ff284e
rherrajan <currant@gmx.de>
2015-02-22 16:23:54 +0100

    aufräumen

--- increase-android/src/main/java/tk/icudi/MobileArrayAdapter.java
+++ increase-android/src/main/java/tk/icudi/MobileArrayAdapter.java
@@ -7,32 +7,32 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
- 
+
 public class MobileArrayAdapter extends ArrayAdapter<String> {
 	private final Context context;
 	private final String[] values;
- 
+
 	public MobileArrayAdapter(Context context, String[] values) {
 		super(context, R.layout.list_mobile, values);
 		this.context = context;
 		this.values = values;
 	}
- 
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		LayoutInflater inflater = (LayoutInflater) context
-			.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
- 
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
 		View rowView = inflater.inflate(R.layout.list_mobile, parent, false);
 		TextView textView = (TextView) rowView.findViewById(R.id.label);
 		ImageView imageView = (ImageView) rowView.findViewById(R.id.logo);
 		textView.setText(values[position]);
- 
+
 		// Change icon based on name
 		String s = values[position];
- 
+
 		System.out.println(s);
- 
+
 		if (s.equals("WindowsMobile")) {
 			imageView.setImageResource(R.drawable.ic_launcher);
 		} else if (s.equals("iOS")) {
@@ -42,7 +42,7 @@ public class MobileArrayAdapter extends ArrayAdapter<String> {
 		} else {
 			imageView.setImageResource(R.drawable.ic_launcher);
 		}
- 
+
 		return rowView;
 	}
 }
