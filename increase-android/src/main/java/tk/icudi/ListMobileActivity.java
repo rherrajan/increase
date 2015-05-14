a53e8d7a2011168a80e11829dff91ad1f43f6ea5
rherrajan <annain@unterderbruecke.de>
2015-05-14 13:36:47 +0200

    dynamische Fragments

--- increase-android/src/main/java/tk/icudi/ListMobileActivity.java
+++ increase-android/src/main/java/tk/icudi/ListMobileActivity.java
@@ -2,13 +2,30 @@ package tk.icudi;
 
 import roboguice.activity.RoboFragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
 public class ListMobileActivity extends RoboFragmentActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
-    }
-    
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_layout);
+
+		activateFragment(new ListAgentsFragment(), savedInstanceState);
+	}
+
+	private void activateFragment(Fragment fragment, Bundle savedInstanceState) {
+
+		if (findViewById(R.id.fragment_container) == null) {
+			return;
+		}
+
+		if (savedInstanceState != null) {
+			return;
+		}
+
+		fragment.setArguments(getIntent().getExtras());
+		getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, fragment).commit();
+	}
+
 }
