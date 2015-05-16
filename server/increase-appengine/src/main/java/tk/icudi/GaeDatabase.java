42e490d42a18c0109254b22b9a70e2905073c9b4
rherrajan <annain@unterderbruecke.de>
2015-05-16 19:27:54 +0200

    diesmal werden wirklich die älteten gelöscht

--- server/increase-appengine/src/main/java/tk/icudi/GaeDatabase.java
+++ server/increase-appengine/src/main/java/tk/icudi/GaeDatabase.java
@@ -11,6 +11,7 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
 
 public class GaeDatabase implements Database {
@@ -61,7 +62,7 @@ public class GaeDatabase implements Database {
 	@Override
 	public void delete(Schema schema, int itemsToKeep) {
 
-		final Query query = new Query(schema.name());
+		final Query query = new Query(schema.name()).addSort("time", SortDirection.DESCENDING);
 		query.setKeysOnly();
 		final List<Key> keys = new ArrayList<Key>();
 		for (final Entity entity : datastore.prepare(query).asIterable(FetchOptions.Builder.withLimit(100000))) {
