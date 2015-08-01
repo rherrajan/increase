dc26cc3c339a81f552a9272d19c929d2ee85efff
rherrajan <annain@unterderbruecke.de>
2015-08-01 15:58:22 +0200

    eigene Toolbar mit icon

--- increase_app/app/src/main/java/tk/icudi/increase/ToolbarFragment.java
+++ increase_app/app/src/main/java/tk/icudi/increase/ToolbarFragment.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +16,7 @@ import tk.icudi.increase.R;
 public class ToolbarFragment extends Fragment {
 
     private View view;
-    private Activity activity;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        this.view = inflater.inflate(R.layout.fragment_toolbar, container, false);
-        System.out.println(" --- onCreateView --- ");
-        return view;
-    }
+    private AppCompatActivity activity;
 
     @Override
     public void onAttach(Activity activity) {
@@ -29,7 +24,19 @@ public class ToolbarFragment extends Fragment {
 
         System.out.println(" --- onAttach --- ");
 
-        this.activity = activity;
+        this.activity = (AppCompatActivity)activity;
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        this.view = inflater.inflate(R.layout.fragment_toolbar, container, false);
+        System.out.println(" --- onCreateView --- ");
+
+        Toolbar toolbar = (Toolbar)view.findViewById(R.id.agent_toolbar);
+        activity.setSupportActionBar(toolbar);
+
+        return view;
+    }
+
 
 }
