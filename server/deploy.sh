af84425fa2f2d14c1136a44fdb370cc31fff39c4
rherrajan <annain@unterderbruecke.de>
2015-10-25 10:39:19 +0100

    deploy für alles auf einmal

---
+++ server/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+check_errs() {
+  # Function. Parameter 1 is the return code
+  # Para. 2 is text to display on failure.
+  if [ "${1}" -ne "0" ]; then
+    echo "ERROR # ${1} : ${2}"
+    # as a bonus, make our script exit with the right error code.
+    exit ${1}
+  fi
+}
+
+
+mvn --projects increase-web --also-make install 
+check_errs $? "error in increase-web"
+
+mvn --projects increase-appengine appengine:update
+check_errs $? "error in increase-appengine"
+
+cd increase-gatherer
+mvn clean appengine:update
+check_errs $? "error in increase-gatherer"
+
+curl http://increase-gatherer.appspot.com/player/send
