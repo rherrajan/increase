e903d78d2888b3d16ffc28a7f54b5c8b22ee2e33
rherrajan <annain@unterderbruecke.de>
2015-10-25 12:01:29 +0100

    htmlunit wieder entfernt

--- server/increase-gatherer/src/main/java/tk/icudi/increase/DriverFactory.java
+++ server/increase-gatherer/src/main/java/tk/icudi/increase/DriverFactory.java
@@ -1,109 +1,109 @@
-package tk.icudi.increase;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.BrowserVersionFeatures;
-import com.gargoylesoftware.htmlunit.CookieManager;
-import com.gargoylesoftware.htmlunit.DefaultCredentialsProvider;
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.HttpMethod;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebRequest;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
-public class DriverFactory {
-
-	private static DriverFactory instance = new DriverFactory();
-	private HTMLDriver htmlUnitDriver;
-
-	public static class HTMLDriver extends HtmlUnitDriver {
-
-		private WebClient webClient;
-
-		public HTMLDriver(BrowserVersion browser) {
-			super(browser);
-		}
-
-		@Override
-		protected WebClient modifyWebClient(WebClient client) {
-
-			this.webClient = client;
-			client.setCookieManager(new CookieManager() {
-
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				protected int getPort(java.net.URL url) {
-					final int r = super.getPort(url);
-					return r != -1 ? r : 80;
-				}
-
-			});
-			
-	        final DefaultCredentialsProvider credentialsProvider = (DefaultCredentialsProvider) client.getCredentialsProvider();
-	        credentialsProvider.addCredentials("vpn", "vpn");
-	        
-			return super.modifyWebClient(client);
-		}
-
-		@Override
-		protected void get(URL fullUrl) {
-			super.get(fullUrl);
-		}
-
-		public HtmlPage getPost(String urlString, Map<String, String> requestParameter, String requestBody) throws FailingHttpStatusCodeException, IOException {
-
-			WebRequest requestSettings = new WebRequest(new URL(urlString), HttpMethod.POST);
-
-			for (Entry<String, String> entry : requestParameter.entrySet()) {
-				requestSettings.setAdditionalHeader(entry.getKey(), entry.getValue());
-			}
-
-			requestSettings.setRequestBody(requestBody);
-			
-			HtmlPage page = webClient.getPage(requestSettings);
-			return page;
-		}
-		
-		
-	}
-
-	public DriverFactory() {
-
-		String applicationName = "Netscape";
-		String applicationVersion = "5.0 (X11)";
-		String userAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0";
-		int browserVersionNumeric = 36;
-
-		BrowserVersion browser = new BrowserVersion(applicationName, applicationVersion, userAgent, browserVersionNumeric) {
-
-			private static final long serialVersionUID = 1L;
-
-			public boolean hasFeature(BrowserVersionFeatures property) {
-//				return BrowserVersion.FIREFOX_38.hasFeature(property);
-				return BrowserVersion.getDefault().hasFeature(property);
-			}
-		};
-
-		this.htmlUnitDriver = new HTMLDriver(browser);
-		htmlUnitDriver.setJavascriptEnabled(true);
-//		htmlUnitDriver.setJavascriptEnabled(false);
-
-		// htmlUnitDriver.setProxy("176.31.99.80", 2222);
-	}
-
-	public static DriverFactory getInstance() {
-		return instance;
-	}
-
-	public HTMLDriver getHTMLUnitDriver() {
-		return htmlUnitDriver;
-	}
-
-}
+//package tk.icudi.increase;
+//
+//import java.io.IOException;
+//import java.net.URL;
+//import java.util.Map;
+//import java.util.Map.Entry;
+//
+//import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+//
+//import com.gargoylesoftware.htmlunit.BrowserVersion;
+//import com.gargoylesoftware.htmlunit.BrowserVersionFeatures;
+//import com.gargoylesoftware.htmlunit.CookieManager;
+//import com.gargoylesoftware.htmlunit.DefaultCredentialsProvider;
+//import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+//import com.gargoylesoftware.htmlunit.HttpMethod;
+//import com.gargoylesoftware.htmlunit.WebClient;
+//import com.gargoylesoftware.htmlunit.WebRequest;
+//import com.gargoylesoftware.htmlunit.html.HtmlPage;
+//
+//public class DriverFactory {
+//
+//	private static DriverFactory instance = new DriverFactory();
+//	private HTMLDriver htmlUnitDriver;
+//
+//	public static class HTMLDriver extends HtmlUnitDriver {
+//
+//		private WebClient webClient;
+//
+//		public HTMLDriver(BrowserVersion browser) {
+//			super(browser);
+//		}
+//
+//		@Override
+//		protected WebClient modifyWebClient(WebClient client) {
+//
+//			this.webClient = client;
+//			client.setCookieManager(new CookieManager() {
+//
+//				private static final long serialVersionUID = 1L;
+//
+//				@Override
+//				protected int getPort(java.net.URL url) {
+//					final int r = super.getPort(url);
+//					return r != -1 ? r : 80;
+//				}
+//
+//			});
+//			
+//	        final DefaultCredentialsProvider credentialsProvider = (DefaultCredentialsProvider) client.getCredentialsProvider();
+//	        credentialsProvider.addCredentials("vpn", "vpn");
+//	        
+//			return super.modifyWebClient(client);
+//		}
+//
+//		@Override
+//		protected void get(URL fullUrl) {
+//			super.get(fullUrl);
+//		}
+//
+//		public HtmlPage getPost(String urlString, Map<String, String> requestParameter, String requestBody) throws FailingHttpStatusCodeException, IOException {
+//
+//			WebRequest requestSettings = new WebRequest(new URL(urlString), HttpMethod.POST);
+//
+//			for (Entry<String, String> entry : requestParameter.entrySet()) {
+//				requestSettings.setAdditionalHeader(entry.getKey(), entry.getValue());
+//			}
+//
+//			requestSettings.setRequestBody(requestBody);
+//			
+//			HtmlPage page = webClient.getPage(requestSettings);
+//			return page;
+//		}
+//		
+//		
+//	}
+//
+//	public DriverFactory() {
+//
+//		String applicationName = "Netscape";
+//		String applicationVersion = "5.0 (X11)";
+//		String userAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0";
+//		int browserVersionNumeric = 36;
+//
+//		BrowserVersion browser = new BrowserVersion(applicationName, applicationVersion, userAgent, browserVersionNumeric) {
+//
+//			private static final long serialVersionUID = 1L;
+//
+//			public boolean hasFeature(BrowserVersionFeatures property) {
+////				return BrowserVersion.FIREFOX_38.hasFeature(property);
+//				return BrowserVersion.getDefault().hasFeature(property);
+//			}
+//		};
+//
+//		this.htmlUnitDriver = new HTMLDriver(browser);
+//		htmlUnitDriver.setJavascriptEnabled(true);
+////		htmlUnitDriver.setJavascriptEnabled(false);
+//
+//		// htmlUnitDriver.setProxy("176.31.99.80", 2222);
+//	}
+//
+//	public static DriverFactory getInstance() {
+//		return instance;
+//	}
+//
+//	public HTMLDriver getHTMLUnitDriver() {
+//		return htmlUnitDriver;
+//	}
+//
+//}
--- server/increase-gatherer/src/main/java/tk/icudi/increase/LogProviderGAEWeb.java
+++ server/increase-gatherer/src/main/java/tk/icudi/increase/LogProviderGAEWeb.java
@@ -1,74 +1,74 @@
-package tk.icudi.increase;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import tk.icudi.LogProviderWeb;
-import tk.icudi.RequestData;
-
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.CookieManager;
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.HttpMethod;
-import com.gargoylesoftware.htmlunit.Page;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebRequest;
-
-public class LogProviderGAEWeb extends LogProviderWeb {
-
-	private WebClient webClient;
-
-	public LogProviderGAEWeb(RequestData data) {
-		super(data);
-
-		webClient = new WebClient(BrowserVersion.getDefault());
-//		webClient = new WebClient(BrowserVersion.getDefault(),"176.31.99.80", 2222);
-		
-		webClient.getOptions().setUseInsecureSSL(true);
-		webClient.getOptions().setJavaScriptEnabled(false);
-		webClient.setCookieManager(new CookieManager() {
-
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			protected int getPort(java.net.URL url) {
-				final int r = super.getPort(url);
-				return r != -1 ? r : 80;
-			}
-
-		});
-	}
-
-
-	@Override
-	protected InputStream provideLogs(Map<String, String> headerParameter, String postBody) throws MalformedURLException, IOException, ProtocolException {
-		
-		String urlString = "https://lienz.lima.zone/yxorp3.php/https://www.ingress.com/r/getPlexts";
-
-
-		Page page = openPage(urlString, headerParameter, postBody);
-
-		return page.getWebResponse().getContentAsStream();
-	}
-	
-
-
-	
-	private Page openPage(String urlString, Map<String, String> headerParameter, String requestBody) throws FailingHttpStatusCodeException, IOException {
-
-		WebRequest requestSettings = new WebRequest(new URL(urlString), HttpMethod.POST);
-
-		for (Entry<String, String> entry : headerParameter.entrySet()) {
-			requestSettings.setAdditionalHeader(entry.getKey(), entry.getValue());
-		}
-
-		requestSettings.setRequestBody(requestBody);
-				
-		return webClient.<Page>getPage(requestSettings);
-	}
-}
+//package tk.icudi.increase;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.net.MalformedURLException;
+//import java.net.ProtocolException;
+//import java.net.URL;
+//import java.util.Map;
+//import java.util.Map.Entry;
+//
+//import tk.icudi.LogProviderWeb;
+//import tk.icudi.RequestData;
+//
+//import com.gargoylesoftware.htmlunit.BrowserVersion;
+//import com.gargoylesoftware.htmlunit.CookieManager;
+//import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+//import com.gargoylesoftware.htmlunit.HttpMethod;
+//import com.gargoylesoftware.htmlunit.Page;
+//import com.gargoylesoftware.htmlunit.WebClient;
+//import com.gargoylesoftware.htmlunit.WebRequest;
+//
+//public class LogProviderGAEWeb extends LogProviderWeb {
+//
+//	private WebClient webClient;
+//
+//	public LogProviderGAEWeb(RequestData data) {
+//		super(data);
+//
+//		webClient = new WebClient(BrowserVersion.getDefault());
+////		webClient = new WebClient(BrowserVersion.getDefault(),"176.31.99.80", 2222);
+//		
+//		webClient.getOptions().setUseInsecureSSL(true);
+//		webClient.getOptions().setJavaScriptEnabled(false);
+//		webClient.setCookieManager(new CookieManager() {
+//
+//			private static final long serialVersionUID = 1L;
+//
+//			@Override
+//			protected int getPort(java.net.URL url) {
+//				final int r = super.getPort(url);
+//				return r != -1 ? r : 80;
+//			}
+//
+//		});
+//	}
+//
+//
+//	@Override
+//	protected InputStream provideLogs(Map<String, String> headerParameter, String postBody) throws MalformedURLException, IOException, ProtocolException {
+//		
+//		String urlString = "https://lienz.lima.zone/yxorp3.php/https://www.ingress.com/r/getPlexts";
+//
+//
+//		Page page = openPage(urlString, headerParameter, postBody);
+//
+//		return page.getWebResponse().getContentAsStream();
+//	}
+//	
+//
+//
+//	
+//	private Page openPage(String urlString, Map<String, String> headerParameter, String requestBody) throws FailingHttpStatusCodeException, IOException {
+//
+//		WebRequest requestSettings = new WebRequest(new URL(urlString), HttpMethod.POST);
+//
+//		for (Entry<String, String> entry : headerParameter.entrySet()) {
+//			requestSettings.setAdditionalHeader(entry.getKey(), entry.getValue());
+//		}
+//
+//		requestSettings.setRequestBody(requestBody);
+//				
+//		return webClient.<Page>getPage(requestSettings);
+//	}
+//}
